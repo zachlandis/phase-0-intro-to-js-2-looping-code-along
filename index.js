90ccfa69a119628a9b01ad271ffece7af23c27f9
Zach <zachlandis91@gmail.com>
2022-12-19 13:02:23 -0700

    commit tree 6122b660500e65413ed927701748f3deeac0805a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const newArray = [];
+
+function writeCards(name, eventName) {
+    for (let i = 0; i < name.length; i++) {
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+    }
+
+    return newArray;
+}
+
+
+function countDown(num) {
+    let i = 0;
+    while (i <= num) {
+        console.log(num)
+        num--
+    }
+}
